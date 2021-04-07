30ab77d137d4c4bdd9382247ee3bac01e42afe0f
Domenico159 <domenico.zummo@gmail.com>
2021-04-07 14:39:28 +0200

    Project Completed

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+// Focalizzare le referenze 
+
+var hamburger = $('a i.fas.fa-bars') ;
+
+var sidebar = $('.hamburger-menu');
+
+var closeSidebar = $('.close');
+
+
+// Logica
+
+// Apertura al click dell'hamburger
+
+hamburger.click( function() {
+
+   sidebar.fadeIn();
+
+} );
+
+// Chiusura al click dell'hamburger
+
+closeSidebar.click( function() {
+
+    sidebar.fadeOut();
+ 
+ } );
